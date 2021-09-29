fc24f428ede8d5bcecd3293aa28fd73b0df680ec
Cheatoid <Cheatoid@users.noreply.github.com>
2021-09-29 10:12:38 +0200

    Refining...

--- lua/entities/gmod_wire_expression2/core/unknown.lua
+++ lua/entities/gmod_wire_expression2/core/unknown.lua
@@ -1,4 +1,4 @@
-local UNKNOWN_TYPENAME, UNKNOWN_TYPEID, createUnknown = "unknown", "xxx", E2Lib.createUnknown
+local UNKNOWN_TYPENAME, UNKNOWN_TYPEID, createUnknown, isUnknown = "unknown", "xxx", E2Lib.createUnknown, E2Lib.isUnknown
 
 -- This is a special type (for advanced users only).
 -- Sole purpose of bypassing type checker and being able to pass table's values directly when doing stringcalls.
@@ -6,14 +6,14 @@ registerType("unknown", "xxx", nil,
 	nil,
 	nil,
 	function(retval)
-		if retval == nil or istable(retval) then return end
+		if retval == nil or isUnknown(retval) then return end
 		error(string.format("Return value is not '%s', but a '%s'!", UNKNOWN_TYPENAME, type(retval)), 0)
 	end,
 	function(v)
 		-- If typecheck returns true, the type is wrong.
-		-- So, we allow every value to 'downcast' to unknown by returning false.
+		-- So, we allow every value to 'downcast' to unknown by returning false... actually not really since we bypass typecheck.
 		print("************* typeof v:", type(v)) -- REMOVEME
-		return false --v ~= nil or not istable(v)
+		return not isUnknown(v)
 	end
 )
 
@@ -32,7 +32,7 @@ __e2setcost(1)
 
 --- if (unknown)
 e2function number operator_is(unknown xxx)
-	return istable(xxx) and 1 or 0
+	return isUnknown(xxx) and 1 or 0
 end
 
 --- XXX == XXX
@@ -48,27 +48,29 @@ end
 __e2setcost(5)
 
 e2function unknown table:operator[](index)
-	local value = this.n[index]
-	if value == nil then return nil end
-	return createUnknown(this.ntypes[index], value)
+	local typeid = this.ntypes[index]
+	if typeid == nil then return nil end
+	return createUnknown(typeid, this.n[index])
 end
 
 e2function unknown table:operator[](string key)
-	local value = this.s[key]
-	if value == nil then return nil end
-	return createUnknown(this.stypes[key], value)
+	local typeid = this.stypes[key]
+	if typeid == nil then return nil end
+	return createUnknown(typeid, this.s[key])
 end
 
 __e2setcost(1)
 
---- Return invalid/default (unknown) value.
+--- Returns invalid/default (unknown) value.
 e2function unknown nounknown()
 	return nil
 end
 
---- Retrieve value's internal typeid.
+__e2setcost(2)
+
+--- Retrieves internal value's typeid.
 e2function string unknown:typeid()
-	return this and this[1] or ""
+	return isUnknown(this) and this[1] or ""
 end
 
 __e2setcost(3)
@@ -86,14 +88,10 @@ end
 
 registerCallback("postinit", function()
 	local fixDefault, fixNormal = E2Lib.fixDefault, E2Lib.fixNormal
-	local skip_types = {
-		--xgt = true;
-		xxx = true;
-	}
 	__e2setcost(5)
-	for typeName, v in next, wire_expression_types do
-		local typeid = v[1]
-		if skip_types[typeid] then continue end
+	for typeName, e2type in next, wire_expression_types do
+		local typeid = e2type[1]
+		if typeid == UNKNOWN_TYPEID then continue end
 
 		-- XXX = unknown(<Value>)
 		registerFunction(UNKNOWN_TYPENAME, typeid, UNKNOWN_TYPEID, function(self, args)
@@ -102,13 +100,13 @@ registerCallback("postinit", function()
 			return createUnknown(typeid, value)
 		end)
 
-		local name, defaultValue = fixNormal(typeName), v[2]
+		local name, defaultValue = fixNormal(typeName), e2type[2]
 
 		-- Value = XXX:<type>()
 		registerFunction(name, UNKNOWN_TYPEID .. ":", typeid, function(self, args)
 			local op1 = args[2]
-			local obj = op1[1](self, op1)
-			return obj[1] == typeid and obj[2] or fixDefault(defaultValue) -- Type check
+			local xxx = op1[1](self, op1)
+			return xxx[1] == typeid and xxx[2] or fixDefault(defaultValue) -- Type check
 		end)
 	end
 end)
@@ -122,12 +120,18 @@ local T = table(
     ("Epic") = "Very epic.",
     (9000) = 9001
 )
-F(T[1337])   # same thing as T[1337, unknown]
-F(T["Epic"]) # same thing as T["Epic", unknown]
-F(T[9000])   # same thing as T[9000, unknown]
+local XXX = T[1337] # equivalent to T[1337, unknown]
+F(XXX)
+F(T["Epic"])
+F(T[9000])
+
+print("XXX:typeid() = " + XXX:typeid())
+print("XXX:isValid() = " + XXX:isValid())
 
 # Output:
 # [myFunc(s)] Hello from unknown
 # [myFunc(s)] Very epic.
 # [myFunc(n)] Num=9001
+# XXX:typeid() = s
+# XXX:isValid() = 1
 ]]
